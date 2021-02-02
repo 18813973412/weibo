612d79a162aeabfbc4c3f9b588dd6fad927f1efe
Lin <546594050@qq.com>
2021-02-02 02:59:18 +0000

    完成用户注册功能

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,7 +1,4 @@
 <footer class="footer">
- <a href="#" target=_blank>
-    刻意练习，每日精进
-    </a>
     <div class="float-right">
         <a href="{{ route('about') }}" >关于</a>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@
 */
 Route::get('/', 'StaticPagesController@home');
 Route::get('/help', 'StaticPagesController@help');
-Route::get('/about', 'StaticPagesController@about');
+Route::get('/about', 'StaticPagesController@about')->name('about');
 
 Route::get('signup', 'UsersController@create')->name('signup');
 Route::resource('users', 'UsersController');
